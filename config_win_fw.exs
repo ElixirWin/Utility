f28cf61faa9768062bf05217b777b6abf3d54776
Onorio Catenacci <OnorioCatenacci@quickenloans.com>
2014-04-21 10:57:48 -0400

    Removing unneeded @doc lines and adding code to make this module work with gen_ server

--- config_win_fw.exs
+++ config_win_fw.exs
@@ -1,25 +1,23 @@
 defmodule ConfigWinFirewallForElixir do
     import System
+    use Application.Behaviour
+
 @moduledoc """
 Configure the windows firewall to allow Erlang/Elixir traffic to pass through
 """
-    @doc "return the default path to the netsh executable"
     defp path_to_netsh do 
         root = get_env("SystemRoot")
         root <> "/system32/netsh.exe"
     end
 
-    @doc "return a correctly formed netsch command"
     defp get_correctly_formed_netsh_command(command_argument) do
         " advfirewall firewall " <> command_argument
     end
 
-    @doc "build the whole netsh command"
     defp build_netsh_command(subcommand) do
         path_to_netsh <> get_correctly_formed_netsh_command(subcommand)
     end        
 
-    @doc "simply check if there is already an existing rule"
     defp firewall_rule_already_exists?(rulename) do
         test_for_rule_args = "show rule " <> rulename
         result = cmd(build_netsh_command(test_for_rule_args))
@@ -33,5 +31,9 @@ Configure the windows firewall to allow Erlang/Elixir traffic to pass through
             add_rule_cmd = "add rule name=" <> rulename <> " dir=out action=allow program=" <> executable <> " profile=domain"
             result = cmd(build_netsh_command(add_rule_cmd))
         end
+    end
+
+    def start(_type, _args) do
+        ConfigWinFirewallForElixir.Supervisor.start_link
     end
 end    
